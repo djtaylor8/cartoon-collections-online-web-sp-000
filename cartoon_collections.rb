363b1517e9ded9f3ebd59fb6f3a34a99b4852deb
DJ Taylor <dtaylor88@gmail.com>
2021-06-01 03:25:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,5 +20,5 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   #cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find {|item| item = "cheddar" || item = "gouda" || item = "camembert"}
+  array.find {|item| item == "cheddar" || item == "gouda" || item == "camembert"}
 end
